3b056c347ba1944fc30608a04fedd5beea13660d
Ian Lam <ljy200523@gmail.com>
2026-02-26 10:31:04 +0800

    Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 3, 5))
 
 // Do not edit below this line
 module.exports = removeFromArray;
