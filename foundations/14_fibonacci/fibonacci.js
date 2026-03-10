42437b7dc3cd05bca2621317714b8a6b74d5041e
Ian Lam <ljy200523@gmail.com>
2026-03-10 16:20:17 +0800

    Progress on 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let a = 0;
+    let b = 1;
+    let runningCount = 0;
+    for(let i = 0; i < number; i++) {
+        console.log("a = ", a);
+        console.log("b = ", b);
+        runningCount = (a + b);
+        a = b;
+        b = runningCount;
+        console.log("runningCount = ", runningCount);
+    }
+    return runningCount;
 };
+console.log(fibonacci(3));
 
 // Do not edit below this line
 module.exports = fibonacci;
