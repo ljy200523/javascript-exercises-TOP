b8c10a6558dd1c383fc32eacea13a68f1d5751f8
Ian Lam <ljy200523@gmail.com>
2026-03-10 12:17:41 +0800

    Complete 12_calculator
    note "for (let i = 1; i == number; i++)" does not work

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let runningTotal = 1;
+	for (let i = 1; i < number + 1; i++) {
+    runningTotal *= i;
+  }
+  return runningTotal;
 };
 
 // Do not edit below this line
