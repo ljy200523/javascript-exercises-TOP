8d4be40820ab92a903617643143fafc355a02466
Ian Lam <ljy200523@gmail.com>
2026-03-10 14:35:00 +0800

    Complete 13_palindromes
    Convert string to array (.split), reverse, array back to string (.join)
    Use regex to clean string

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversedString = cleanString.split("").reverse().join("");
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
