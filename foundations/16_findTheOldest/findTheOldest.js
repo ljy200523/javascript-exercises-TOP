44e28724cceeafaffa3e2cbf10ee6f89db8f2026
Ian Lam <ljy200523@gmail.com>
2026-03-11 14:18:41 +0800

    Complete 16_findTheOldest
    Use Date() to get date if undefined

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfObjects) {
+    let newArray = arrayOfObjects.reduce((accumulator, currentValue) => {
+        if (currentValue.yearOfDeath === undefined) {
+            currentValue.yearOfDeath = new Date().getFullYear();
+        }
+        accumulator.push(
+            {name: currentValue.name, 
+            years: (currentValue.yearOfDeath - currentValue.yearOfBirth)
+            });
+        return accumulator;
+    }, [])
+    newArray.sort((a, b) => b.years - a.years);
+    return newArray[0];
 };
 
 // Do not edit below this line
